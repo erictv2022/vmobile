ee27151468b8ad7b755fa78caecf78413be47701
Eric Yu <217014110@stu.vtc.edu.hk>
2022-06-26 17:26:28 +0800

    feat: take photo as profile picture

--- Fishes/app/src/main/java/co/cueric/fishes/features/authentication/profile/ProfileScreen.kt
+++ Fishes/app/src/main/java/co/cueric/fishes/features/authentication/profile/ProfileScreen.kt
@@ -1,20 +1,31 @@
 package co.cueric.fishes.features.authentication.profile
 
+import android.net.Uri
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Cancel
 import androidx.compose.material.icons.filled.Done
 import androidx.compose.material.icons.filled.Edit
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import co.cueric.fishes.core.compose.ComposeFileProvider
+import coil.compose.AsyncImage
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.consumeAsFlow
 
 @Composable
 fun ProfileScreen(navController: NavController, viewModel: UserProfileViewModel) {
@@ -22,16 +33,53 @@ fun ProfileScreen(navController: NavController, viewModel: UserProfileViewModel)
     val isEditing by viewModel.isEditing.collectAsState()
     val error by viewModel.error.collectAsState(initial = null)
     val showAlertDialog by viewModel.showAlertDialog.collectAsState()
+    val context = LocalContext.current
+    val cameraPermission by viewModel.cameraPermissionGranted.collectAsState()
 
-    if (showAlertDialog && error != null){
+    //region camera
+    var hasImage by remember {
+        mutableStateOf(false)
+    }
+    var imageUri by remember {
+        mutableStateOf<Uri?>(null)
+    }
+
+    val imagePicker = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.GetContent(),
+        onResult = { uri ->
+            hasImage = uri != null
+            imageUri = uri
+        }
+    )
+
+    val cameraLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.TakePicture(),
+        onResult = { success ->
+            hasImage = success
+        }
+    )
+
+    LaunchedEffect(key1 = Unit) {
+        viewModel.cameraPermissionGranted.collectLatest { cameraPermission ->
+            if (cameraPermission){
+                viewModel.updateCameraPermission(false)
+                val uri = ComposeFileProvider.getImageUri(context)
+                imageUri = uri
+                cameraLauncher.launch(uri)
+            }
+        }
+    }
+    //endregion
+
+    if (showAlertDialog && error != null) {
         AlertDialog(
             onDismissRequest = { viewModel.dismissAlertDialog() },
             confirmButton = {
-                TextButton(onClick = { viewModel.dismissAlertDialog()})
+                TextButton(onClick = { viewModel.dismissAlertDialog() })
                 { Text(text = "OK") }
             },
-            title = { Text(text = "Error")},
-            text = { Text(text = error?.message.orEmpty())}
+            title = { Text(text = "Error") },
+            text = { Text(text = error?.message.orEmpty()) }
         )
     }
     
@@ -67,8 +115,21 @@ fun ProfileScreen(navController: NavController, viewModel: UserProfileViewModel)
                         .padding(16.dp)
                         .fillMaxWidth(),
                 ) {
-                    Row(modifier = Modifier.fillMaxWidth()) {
-                        //profile pic
+                    if (hasImage && imageUri != null) {
+                        AsyncImage(
+                            model = imageUri,
+                            modifier = Modifier
+                                .size(120.dp)
+                                .clip(CircleShape)
+                                .background(Color.LightGray)
+                                .border(1.dp, Color.LightGray),
+                            contentDescription = "profile image",
+                        )
+                    }
+                    Button(onClick = {
+                        viewModel.takePhoto()
+                    }) {
+                        Text(text = "Change Profile Photo")
                     }
 
                     OutlinedTextField(
@@ -77,7 +138,7 @@ fun ProfileScreen(navController: NavController, viewModel: UserProfileViewModel)
                         label = { Text("Display Name") },
                         readOnly = !isEditing
                     )
-                    
+
                     TextButton(onClick = { viewModel.logout() }) {
                         Text("Sign Out")
                     }
