4d5a716e8cb4a48423bbf22796a348eca306ea56
Eric Yu <217014110@stu.vtc.edu.hk>
2022-06-26 22:39:53 +0800

    add error code documentation

--- Fishes/app/src/main/java/co/cueric/fishes/core/errors/BaseError.kt
+++ Fishes/app/src/main/java/co/cueric/fishes/core/errors/BaseError.kt
@@ -1,8 +1,18 @@
 package co.cueric.fishes.core.errors
 
+/**
+ * ErrorCode enum
+ *
+ */
 enum class ERRORCODE {
     FAIL,
     OK
 }
 
+/**
+ * Base class for error
+ *
+ * @property errorCode
+ * @property message Message to show for user
+ */
 open class BaseError(open val errorCode: Int, open val message: String?)
