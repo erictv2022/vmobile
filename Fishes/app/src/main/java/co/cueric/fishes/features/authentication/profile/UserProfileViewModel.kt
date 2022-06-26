780e285bef2aba2e1d36fcd9914d46c3249dc82f
Eric Yu <217014110@stu.vtc.edu.hk>
2022-06-26 21:02:56 +0800

    feat: update current gps location and reverse to address as default delivery address

--- Fishes/app/src/main/java/co/cueric/fishes/features/authentication/profile/UserProfileViewModel.kt
+++ Fishes/app/src/main/java/co/cueric/fishes/features/authentication/profile/UserProfileViewModel.kt
@@ -1,15 +1,22 @@
 package co.cueric.fishes.features.authentication.profile
 
 import android.app.Application
+import android.content.Context
 import android.location.Location
 import android.util.Log
 import androidx.lifecycle.viewModelScope
+import co.cueric.fishes.config.locationiqAPIKey
 import co.cueric.fishes.core.BaseViewModel
 import co.cueric.fishes.core.STATEFLOW_STARTED
 import co.cueric.fishes.core.errors.AuthenticationError
+import co.cueric.fishes.core.errors.BaseError
 import co.cueric.fishes.core.errors.DataError
 import co.cueric.fishes.core.errors.ERRORCODE
 import co.cueric.fishes.managers.AuthManager
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
 import com.google.firebase.auth.ktx.userProfileChangeRequest
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -75,6 +82,14 @@ class UserProfileViewModel(application: Application) :BaseViewModel(application)
         _displayName.update { name }
     }
 
+    fun updateAddress1(address: String?){
+        _addressLine1.update { address.orEmpty() }
+    }
+
+    fun updateAddress2(address: String?){
+        _addressLine2.update { address.orEmpty() }
+    }
+
     fun saveUserProfile() {
         showLoading()
         val userReq = userProfileChangeRequest {
@@ -114,8 +129,47 @@ class UserProfileViewModel(application: Application) :BaseViewModel(application)
         locationPermissionGranted.update { granted }
     }
 
-    fun updateLocation(location: Location?) {
+    fun updateLocation(location: Location?, context: Context) {
         currentLocation.update { location }
+        viewModelScope.launch {
+            reverseGeoLocation(
+                lat = location?.latitude.toString(),
+                long = location?.longitude.toString(),
+                context = context
+            )
+        }
+    }
+
+    fun reverseGeoLocation(lat: String, long: String, context: Context) {
+        val queue = Volley.newRequestQueue(context)
+        val url =
+            "https://us1.locationiq.com/v1/reverse?key=${locationiqAPIKey}&lat=${lat}&lon=${long}&format=json"
+
+        val jsonObjectRequest = JsonObjectRequest(Request.Method.GET, url, null,
+            Response.Listener { response ->
+                val address = response.getJSONObject("address")
+                _addressLine1.update {
+                    val address = response.getString("display_name").split(',').take(2).joinToString(",")
+                    address
+                }
+                _addressLine2.update {
+                    val address = "${address["road"]}, ${address["city"]}, ${address["state"]}, ${address["country"]}"
+                   address
+                }
+                "Response: %s".format(response.toString())
+            },
+            Response.ErrorListener { error ->
+                showError(
+                    BaseError(
+                        errorCode = error.networkResponse.statusCode,
+                        message = error.localizedMessage
+                    )
+                )
+            }
+        )
+
+        // Add the request to the RequestQueue.
+        queue.add(jsonObjectRequest)
     }
 
     fun logout() {
